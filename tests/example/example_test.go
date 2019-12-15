d1da7cb3d9242adb1ffc82497926c92f96ce16f9
mmmknt <mmmknt@mmmknt.dev>
2019-12-16 02:06:45 +0900

    Update example_test.go

--- tests/example/example_test.go
+++ tests/example/example_test.go
@@ -14,8 +14,8 @@ func TestParse(t *testing.T) {
 	err := o.ExecParse("test.localhost:8888/path")
 	assert.NoError(t, err)
 	data := *o.GetTmpData()
-	assert.Equal(t, "test", data["userkey"][0])
-	assert.Equal(t, "path", data["userpath"][0])
+	assert.Equal(t, "test", data["prefix"][0])
+	assert.Equal(t, "path", data["path"][0])
 	fmt.Print(data)
 
 	err = o.ExecParse("test.invalidhost:aaaa")
